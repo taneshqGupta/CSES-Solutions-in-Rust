79969053ca96aeaac4eb6fff873f6508fa8405af
taneshqGupta <f.taneshq@gmail.com>
2025-07-24 02:10:15 +0530

    update

--- introductory_problems/14_tower_of_hanoi_sol2.rs
+++ introductory_problems/14_tower_of_hanoi_sol2.rs
@@ -2,14 +2,18 @@
 // DateSolved: 18 Jul 2025
 // SolvedBy: taneshqGupta
 
+#[derive(Default, Debug)]
 struct Scanner(Vec<String>);
 impl Scanner {
-    fn new() -> Self {
-        let input = std::io::read_to_string(std::io::stdin()).unwrap();
-        Scanner(input.split_whitespace().map(String::from).rev().collect())
-    }
     fn next<T: std::str::FromStr>(&mut self) -> T {
-        self.0.pop().unwrap().parse().ok().unwrap()
+        loop {
+            if let Some(c) = self.0.pop() {
+                return c.parse().ok().unwrap();
+            }
+            let mut s = String::new();
+            std::io::stdin().read_line(&mut s).unwrap();
+            self.0 = s.split_whitespace().rev().map(String::from).collect();
+        }
     }
 }
 
